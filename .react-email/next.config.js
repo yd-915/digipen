78cd924a5fa55a930483f39fa672a6f0fd025543
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-09-18 12:03:06 -0400

    Update next.config.js

--- .react-email/next.config.js
+++ .react-email/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   experimental: {
     appDir: true,
     externalDir: true, // compile files that are located next to the .react-email directory
